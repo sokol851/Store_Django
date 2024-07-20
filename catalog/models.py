790053ac0a5385f660d89bbf4eb2d386c83fa9a9
Ilia Akatov <ilia-akatoff@ya.ru>
2024-07-21 00:16:30 +0300

    1) Created models Product and Category. 2) Edited settings for DB. 3) Added psycopg2 and pillow in requirements.txt 4) Added settings and urls for media of project. 5) Edited localisation on ru. 6) Created package for custom functions.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='catalog/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    date_create = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='Дата создания')
+    date_edited = models.DateField(auto_now_add=True, blank=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return (f'Наименование: {self.name}\n'
+                f'Описание: {self.description}\n'
+                f'Изображение: {self.description}\n'
+                f'Категория: {self.category}\n'
+                f'Цена: {self.price}\n'
+                f'Дата создания: {self.date_create}\n'
+                f'Дата изменения: {self.date_edited}')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('name',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+
+    def __str__(self):
+        return (f'Наименование: {self.name}\n'
+                f'Описание: {self.description}')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        "PASSWORD": '12345'
     }
 }
 
@@ -100,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +124,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
