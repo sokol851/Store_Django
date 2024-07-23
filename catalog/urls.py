1d51466a6216c52d4724c3e6c63ab21273b84b7e
Ilia Akatov <ilia-akatoff@ya.ru>
2024-07-23 23:33:27 +0300

    Template added for body and menu. Edited pages using with templates. Edited urls in app and pages.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
+from catalog.apps import CatalogConfig
 from catalog.views import index, contacts, feedback
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contacts/', contacts),
-    path('feedback/', feedback),
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('feedback/', feedback, name='feedback'),
 ]
