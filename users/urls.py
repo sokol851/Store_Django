19b045806f46baaab60db04e294434f17eb793fa
Ilia Akatov <ilia-akatoff@ya.ru>
2024-08-12 00:44:44 +0300

    Create page for profile of user. Added update and delete account.

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileUpdateView, ProfileDetailView, ProfileDeleteView
 
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
-
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    # path('reg_confirm', LoginView.as_view(template_name='users/reg_confirm.html'), name='reg_confirm'),
+    path('user_register/', RegisterView.as_view(), name='user_register'),
+    path('user_update/<int:pk>', ProfileUpdateView.as_view(), name='user_update'),
+    path('user_detail/<int:pk>', ProfileDetailView.as_view(), name='user_detail'),
+    path('user_delete/<int:pk>', ProfileDeleteView.as_view(), name='user_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DetailView
+from config import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
 
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
-from users.services import send_user_email
 
 
 class RegisterView(CreateView):
@@ -12,16 +17,46 @@ class RegisterView(CreateView):
     template_name = "users/register.html"
     success_url = reverse_lazy('users:login')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация на сайте'
+        return context
+
     # def form_valid(self, form):
-    #     obj = form.save()
-    #     send_user_email(obj)
-    #     return super().form_valid(form)
+    #     user = form.save(commit=False)
+    #     user.is_active = False
+    #     user.save()
+    #
+    #     token = default_token_generator.make_token(user)
+    #     uid = urlsafe_base64_encode(force_bytes(user.pk))
+    #     activation_url = reverse_lazy('users:reg_done', kwargs={'uidb64': uid, 'token': token})
+    #
+    #     send_mail(
+    #         'Подтвердите регистрацию!',
+    #         (
+    #             f"Благодарим за регистрацию на сайте V-magazine.\n"
+    #             "Для активации учётной записи, пожалуйста перейдите по ссылке:\n"
+    #             f"http://localhost:8000/{activation_url}\n"
+    #         ),
+    #         settings.EMAIL_HOST_USER,
+    #         [user.email],
+    #         fail_silently=False,
+    #     )
+    #     return redirect('email_confirmation_sent')
 
 
-class ProfileView(UpdateView):
+class ProfileDetailView(DetailView):
+    model = User
+
+
+class ProfileDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('catalog:index')
+
+
+class ProfileUpdateView(UpdateView):
     model = User
     form_class = UserProfileForm
-    success_url = reverse_lazy('users:profile')
 
-    def get_object(self, queryset=None):
-        return self.request.user
+    def get_success_url(self):
+        return reverse('users:user_detail', args=[self.kwargs.get('pk')])
