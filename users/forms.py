b61792fa25e87efbad54eac5e86b58fc19c3a841
Ilia Akatov <ilia-akatoff@ya.ru>
2024-08-11 20:59:24 +0300

    Fixed model

--- users/forms.py
+++ users/forms.py
@@ -8,13 +8,13 @@ from users.models import User
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2', )
 
 
 class UserProfileForm(StyleFormMixin, UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country', )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
