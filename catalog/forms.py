d3731df1300095b6276916dd5b36b3fd08205bdc
Ilia Akatov <ilia-akatoff@ya.ru>
2024-08-22 23:23:32 +0300

    Added descriptions for functions of products.

--- catalog/forms.py
+++ catalog/forms.py
@@ -5,6 +5,7 @@ from catalog.models import Product, Version, Feedback
 
 
 class StyleFormMixin:
+    """ Миксин для изменения аттрибутов стилей. """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
@@ -16,6 +17,7 @@ class StyleFormMixin:
 
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
+    """ Форма для продукта """
     list_stop_word = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
                       'радар']
 
@@ -26,6 +28,7 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         # exclude = ('is_active',)
 
     def clean_name(self):
+        """ Фильтрация запрещённых слов в названии """
         clean_data = self.cleaned_data['name']
 
         for word in self.list_stop_word:
@@ -34,6 +37,7 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         return clean_data
 
     def clean_description(self):
+        """ Фильтрация запрещённых слов в описании """
         clean_data = self.cleaned_data['description']
 
         for word in self.list_stop_word:
@@ -43,18 +47,21 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
 
 
 class VersionForm(StyleFormMixin, forms.ModelForm):
+    """ Форма для версий """
     class Meta:
         model = Version
         fields = '__all__'
 
 
 class FeedbackForm(StyleFormMixin, forms.ModelForm):
+    """ Форма для обратной связи """
     class Meta:
         model = Feedback
         fields = ('name', 'email', 'content')
 
 
 class ProdModeratorForm(StyleFormMixin, forms.ModelForm):
+    """ Форма редактирования продукта для модераторов """
     class Meta:
         model = Product
         fields = ("category", "description", "is_active",)
--- catalog/services.py
+++ catalog/services.py
@@ -2,16 +2,17 @@ from django.core.cache import cache
 from django.conf import settings
 
 
-def get_data_from_cache(model, name_list: str):
+def get_data_from_cache(model, name_list: str) -> list[dict]:
+    """ Проверка и загрузка объектов из кеша. """
     if settings.CACHE_ENABLED:
         if cache.get(name_list):
             subject_list = cache.get(name_list)
-            print(f'Получено значение из кеша:{subject_list}')
+            # print(f'Получено значение из кеша:{subject_list}')
         else:
             subject_list = model.objects.all()
             cache.set(name_list, subject_list)
-            print(f'Присвоено новое значение в кеш:{subject_list}')
+            # print(f'Присвоено новое значение в кеш:{subject_list}')
     else:
         subject_list = model.objects.all()
-        print('Кеширование отключено!')
+        # print('Кеширование отключено!')
     return subject_list
