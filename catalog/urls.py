cb1da1868239499b60a76c7c1fa0a47b911cc814
Ilia Akatov <ilia-akatoff@ya.ru>
2024-08-31 18:10:50 +0300

    1) Optimized valid of update for user. 2) Disabled of cache for product list. 3) Optimized model User.

--- catalog/urls.py
+++ catalog/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('create_feedback/', FeedbackCreateView.as_view(), name='create_feedback'),
     path('list_feedback/', FeedbackListView.as_view(), name='list_feedback'),
     path('', ProductListView.as_view(), name='index'),
-    # path('', cache_page(60)(ProductListView.as_view()), name='index'),
     path('detail_prod/<slug:the_slug_prod>', cache_page(60)(ProductDetailView.as_view()), name='product_page'),
     path('update_prod/<slug:the_slug_prod>', ProductUpdateView.as_view(), name='update_product'),
     path('created_prod/', ProductCreateView.as_view(), name='create_product'),
